2d4bc7cf7bfacde417979ca68adef4182553f25d
Vladimir <data-box@mail.com>
2014-08-26 00:51:09 +0400

    second try

--- src/javatutorials/part3/ClassInputData.java
+++ src/javatutorials/part3/ClassInputData.java
@@ -7,19 +7,18 @@ public class ClassInputData {
     public void getData() {
         System.out.print("Введите число или операцию: ");
         Scanner inputVar = new Scanner(System.in);
-        String numVar = inputVar.next();
-        if (inputVar.hasNextInt()){
-            System.out.println("Вы ввели число: " + numVar.toString());
-        } else if (("+").equals(numVar)) {
-            System.out.println("Вы ввели операцию сложения");   
-        } else if (("-").equals(numVar)) {
+        String operationType = inputVar.nextLine();
+        if ((operationType).equals("+")) {
+            System.out.println("Вы ввели операцию сложения"); 
+        } else if ((operationType).equals("-")) {
             System.out.println("Вы ввели операцию вычитания");
-        } else if (("*").equals(numVar)){
+        } else if ((operationType).equals("*")) {
             System.out.println("Вы ввели операцию умножения");
-        } else if (("/").equals(numVar)) {
+        } else if ((operationType).equals("/")) {
             System.out.println("Вы ввели операцию деления");
-        } else {
-            System.out.println("Это не число и не операция");
+        }  else {
+            System.out.println("Это не число и не операция!");
         }
+        
     }
 }
