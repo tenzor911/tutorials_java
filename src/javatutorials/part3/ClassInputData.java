e4db5a6583a5fb518a383a6106dbe2ff2f0f5993
tenzor <toolbar@nm.ru>
2014-08-27 20:09:58 +0400

    new changes 4

--- src/javatutorials/part3/ClassInputData.java
+++ src/javatutorials/part3/ClassInputData.java
@@ -4,58 +4,48 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class ClassInputData {
+    
+    boolean mainCycle = false;
+    int firstNumber; 
+    int secondNumber;
+    int resultNumber;
+    Stack calcStack = new Stack();
+    
+    
     public void getData() {
-        System.out.print("Введите число или операцию: ");
-        Scanner inputVar = new Scanner(System.in);
-        String operationType = inputVar.nextLine();
-        char[] symbolForInput = operationType.trim().toCharArray();
-        Stack calcStack = new Stack();
-
-        String strOfNumbers = "";
-
-        for(int firstCycleCounter = 0; firstCycleCounter < symbolForInput.length; firstCycleCounter++) {
-            if (symbolForInput[firstCycleCounter] >= '0' && symbolForInput[firstCycleCounter] <= '9') {
-                strOfNumbers = strOfNumbers + symbolForInput[firstCycleCounter];
-            } else if (symbolForInput[firstCycleCounter] == '+') {
-                System.out.println("Вы ввели операцию сложения");    
-            } else if (symbolForInput[firstCycleCounter] == '-') {
-                System.out.println("Вы ввели операцию вычитания");
-            } else if (symbolForInput[firstCycleCounter] == '*') {
-                System.out.println("Вы ввели операцию умножения");
-            } else if (symbolForInput[firstCycleCounter] == '/') {
-                System.out.println("Вы ввели операцию деления");
-            } else {
-                System.out.println("ERROR!!!");
-                System.exit(0);
-            }
-        }
-        calcStack.push(Integer.parseInt(strOfNumbers));
-        System.out.println(calcStack.lastElement());
+        while (mainCycle != true) {
+            System.out.print("Введите число или операцию: ");
+            Scanner inputVar = new Scanner(System.in);
+            String operationType = inputVar.nextLine();
+            char[] symbolForInput = operationType.trim().toCharArray();
+            String strOfNumbers = "";
+            for(int firstCycleCounter = 0; firstCycleCounter < symbolForInput.length; firstCycleCounter++) {
+                if (symbolForInput[firstCycleCounter] >= '0' && symbolForInput[firstCycleCounter] <= '9') {
+                    strOfNumbers = strOfNumbers + symbolForInput[firstCycleCounter];
+                    calcStack.push(Integer.parseInt(strOfNumbers));
+                } else if (symbolForInput[firstCycleCounter] == '+') {
+                    System.out.println("Вы ввели операцию сложения");  
+                    action_add();
+                } else if (symbolForInput[firstCycleCounter] == '-') {
+                    System.out.println("Вы ввели операцию вычитания");
+                } else if (symbolForInput[firstCycleCounter] == '*') {
+                    System.out.println("Вы ввели операцию умножения");
+                } else if (symbolForInput[firstCycleCounter] == '/') {
+                    System.out.println("Вы ввели операцию деления");
+                } else if (symbolForInput[firstCycleCounter] == 'e'){
+                    System.out.println("Вы ввели комбинацию для выхода. Программа будет завершена!");
+                    mainCycle = true;
+                } else {
+                    System.out.println("Вы ввели некорректные данные. Программа будет завершена!");
+                    mainCycle = true;
+                }
+            } 
             
-            
-           /*        else if (input[counter] == '+') {
-                System.out.println("Вы ввели операцию сложения");    
-            } else if (input[counter] == '-') {
-                System.out.println("Вычитание");
-            } else if (input[counter] == '*') {
-                System.out.println("Умножение");
-            } else if (input[counter] == '/') {
-                System.out.println("Деление");
-            }
         }
-        
-        if ((input)) {
-            System.out.println("Вы ввели операцию сложения"); 
-        } else if ((operationType).equals("-")) {
-            System.out.println("Вы ввели операцию вычитания");
-        } else if ((operationType).equals("*")) {
-            System.out.println("Вы ввели операцию умножения");
-        } else if ((operationType).equals("/")) {
-            System.out.println("Вы ввели операцию деления");
-        }  else {
-            System.out.println("Это не число и не операция!");
-        }*/
-        }
-
     }
+    void action_add() {
+        System.out.println(calcStack.pop());
+    }
+    
+}
 
